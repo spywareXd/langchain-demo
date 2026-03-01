b7af13d497d3825abf97c017e1e6ee48dc2951ff
Ashwin <rajashwin1909@gmail.com>
2026-03-02 00:28:35 +0530

    formatted search function"

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 import os
 from dotenv import load_dotenv
+from langchain_tavily import TavilySearch
 load_dotenv()
 
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
+from langchain_groq import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
 from tavily import TavilyClient
+from typing import List
+from pydantic import BaseModel, Field
+
+
 tavily=TavilyClient()
 
-@tool
-def search(query : str) -> str:
-    """
-    A tool that is used to search of the internet
-    Args:
-        query: string to be searched on the internet
-    Returns:
-        The search result
-    """
+#pydantic:
+class Source(BaseModel):
+    """Schema for source URL used by agent"""
+    url:str = Field(description="URL of the source")
 
-    print(f"LLM searching for {query}")
-    return tavily.search(query=query)
+class AgentResponse(BaseModel):
+    """Schema for agent response"""
+    answer:str = Field(description="Answer of the agent")
+    sources: List[Source] = Field(default_factory=list, description="Sources of the agent")
 
-tools=[search]
+#@tool
+# def search(query : str) -> str:
+#     """
+#     A tool that is used to search of the internet
+#     Args:
+#         query: string to be searched on the internet
+#     Returns:
+#         The search result
+#     """
+#
+#     print(f"LLM searching for {query}")
+#     return tavily.search(query=query)
 
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
-    temperature=0.7,
-)
-agent=create_agent(model=llm, tools=tools)
 
 
 
+tools=[TavilySearch()]
 
+llm = ChatGoogleGenerativeAI(model="gemini-flash-latest")
+agent=create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
