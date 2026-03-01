91d4fbbb266806446a90f994b9793bb0696e7464
Ashwin <rajashwin1909@gmail.com>
2026-03-01 17:51:13 +0530

    custom search tool

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from tavily import TavilyClient
+tavily=TavilyClient()
+
+@tool
+def search(query : str) -> str:
+    """
+    A tool that is used to search of the internet
+    Args:
+        query: string to be searched on the internet
+    Returns:
+        The search result
+    """
+
+    print(f"LLM searching for {query}")
+    return tavily.search(query=query)
+
+tools=[search]
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7,
+)
+agent=create_agent(model=llm, tools=tools)
+
+
+
+
 
 
 def main():
     print("Hello from langchain-course!")
-
+    result=agent.invoke( {"messages" : [HumanMessage(content="Can you list some job postings for a 3D blender artist in delhi, bangalore or any other city in India? Also tell me what degree to pursure to land such a job.")] } )
+    print(result)
 
 if __name__ == "__main__":
     main()
