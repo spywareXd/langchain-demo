5b088595ebdebff56bca1a444af7f8defad9c7f0
Ashwin <rajashwin1909@gmail.com>
2026-03-07 01:05:59 +0530

    rag without lcel

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from langchain_pinecone import PineconeVectorStore
 
 
 #initialize embeddings and pinecone object
-embeddings=GoogleGenerativeAIEmbeddings(model="gemini-embeddings-001", output_dimensionality=1536)
+embeddings=GoogleGenerativeAIEmbeddings(model="gemini-embedding-001", output_dimensionality=1536)
 vector_store=PineconeVectorStore(index_name=os.environ.get("INDEX_NAME"), embedding=embeddings)
 
 
 #retriever is a vector store with searching capabilities implemented by the vendors
 retriever=vector_store.as_retriever(search_kwargs={"k":3})   #top k chunks where k=3
 
-prompt_template=ChatPromptTemplate(
+prompt_template=ChatPromptTemplate.from_template(
     """
     Answer the question based only on the following context provided:
     {context}
@@ -29,5 +29,35 @@ def format_document(docs):
     """Format documents into a single string"""
     return "\n\n".join(doc.page_content for doc in docs)
 
+
+def rag_without_lcel(query: str):
+    """ Simple retrieval wihout LCEL"""
+
+    llm=ChatGoogleGenerativeAI(model="gemini-flash-latest")
+
+    #retrieve top k chunks of documents
+    #retriever is a runnable object
+    docs=retriever.invoke(query)
+
+    #format top k into string context:
+    context=format_document(docs)
+
+    #format the prompt
+    messages=prompt_template.format_messages(context=context, question=query)
+
+    #run the llm
+    response=llm.invoke(messages)
+    return response.content
+
+
+
+if __name__=="__main__":
+    query="How do vector databases handle massive load?"
+
+    print("=" * 40)
+    print("RAG without LCEL")
+    print("=" * 40)
+    print("\nAnswer: ")
+    print(rag_without_lcel(query)[0].get('text'))
 
 
